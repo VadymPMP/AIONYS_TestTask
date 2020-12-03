e0c8bfe6f03c038ab402aa80b028fbc3aed92289
VadymPMP <kiss15091998@gmail.com>
2020-12-03 23:23:02 +0200

    Test file updated

--- AIONYS_TestTask/ClientApp/cypress/integration/homepage.spec.js
+++ AIONYS_TestTask/ClientApp/cypress/integration/homepage.spec.js
@@ -2,6 +2,7 @@ describe('The home page', () => {
     describe('when visited', () => {
       it('it should open', () => {
         cy.visit('http://localhost:3000/');
+        cy.get('button').contains('en').click();
         cy.get('input').should('have.value', '');
         cy.get('button').contains('Submit').click();
         cy.get('p').should('have.text', 'This field is required');
@@ -9,6 +10,15 @@ describe('The home page', () => {
         cy.get('input').should('have.value', '');
         cy.get('input').type('Hello, today I want to do something');
         cy.get('button').contains('Reset').click();
+        cy.get('input').should('have.value', '');
+        cy.get('button').contains('ру').click();
+        cy.get('input').should('have.value', '');
+        cy.get('button').contains('Отправить').click();
+        cy.get('p').should('have.text', 'Это поле обязательное');
+        cy.get('button').contains('Скинуть').click();
+        cy.get('input').should('have.value', '');
+        cy.get('input').type('Hello, today I want to do something');
+        cy.get('button').contains('Скинуть').click();
         cy.get('input').should('have.value', '');
       })
     })
